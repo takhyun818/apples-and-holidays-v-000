39d5ffa831d6481a134774e63a5ae31c9ea6ccbf
Tak H Kim <takhyun818@gmail.com>
2018-11-01 04:52:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,31 +25,35 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holdiay_hash.each do |season, value|
+  holiday_hash.each do |season, value|
     value[:christmas] = supply
     value[:new_years] = supply
-    #binding.Pry
+    # binding.pry
   end
+
+
 end
+
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  holday_hash.each do |season, value|
-    value:[memorial_day] = supply
+  holiday_hash.each do |season, value|
+    value[:memorial_day] = supply
   end
+
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
- # code here
- # remember to return the updated hash
- holiday_hash.each do |season, value|
-     value[holiday_name] = supply_array
-     # binding.pry
-   end
- end
+  # code here
+  # remember to return the updated hash
+  holiday_hash.each do |season, value|
+    value[holiday_name] = supply_array
+    # binding.pry
+  end
+end
 
- # def all_winter_holiday_supplies(holiday_hash)
+# def all_winter_holiday_supplies(holiday_hash)
 #   # return an array of all of the supplies that are used in the winter season
 #   return holiday_hash[:winter]
 #   binding.pry
